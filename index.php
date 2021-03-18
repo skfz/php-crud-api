dfe60fa665b92b7272fe29f8f9ca2c9b2916ef44
Sarfaraz <skfz.14@gmail.com>
2021-03-18 09:05:36 +0400

    Install php jwt lib

--- index.php
+++ index.php
@@ -2,5 +2,8 @@
 
 require 'bootstrap.php';
 
+//use Nowakowskir\JWT\JWT;
+//use Nowakowskir\JWT\TokenDecoded;
+//use Nowakowskir\JWT\TokenEncoded;
+
 $conn = $dbConnection->connect();
-var_dump($conn);
